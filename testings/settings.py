ea877cfe0aec14033f3eed2e5c1c0a1a37f8efb4
Aayush <aayushgupta6209@gmail.com>
2021-03-12 15:47:35 +0530

    Added admin controls.

--- testings/settings.py
+++ testings/settings.py
@@ -29,8 +29,16 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 INSTALLED_APPS = [
+    'rest_framework',
     'testForm',
     'django.contrib.admin',
     'django.contrib.auth',
